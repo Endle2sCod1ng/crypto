b258f04964f26525b470158c7b56ec62456fb63a
EndlessC0ding <endlesscoding@yahoo.com>
2025-11-23 23:53:47 +0300

    rm sm fls, i tlwnd

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="">
+    dsf
   </header>
 
   <main>
-    <TheWelcome />
+    <h1>Hello Vue 3</h1>
   </main>
 </template>
 
